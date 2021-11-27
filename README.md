9428e7201170ad2907490a4848239dd02850ab69
Mohamed Akram <7mohamedakram@gmail.com>
2021-11-27 17:26:59 +0200

    The Odin Project first Project!

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # rock-paper-scissors
+
+My first project in JavaScript!
+
+In this project i learnt how stupid the computer is and how useful is Chrome DevTools..
+
+i almost banged my head when function (playRound) returns undefined, i tried solving that problem for almost two days and thanks to Chrome DevTools and my good sight jk, i found the solution.
+
+the problem was a capital first letter!!
+
+i read a couple of times that javaScript is case-sensitive but experiencing it myself is something else..
+
+i'll for sure retrun to this project and re-do it when i level up :)
---
+++ script.js
@@ -0,0 +1,63 @@
+const logic = ["Rock", "Paper", "Scissors"];
+
+function computerPlay() {
+    let random = Math.floor(Math.random() * logic.length);
+    return logic[random].toLowerCase();
+}
+
+function playerChoice() {
+    let choice =  prompt("What do you choose? Rock, Paper or Scissors?");
+    return choice.toLowerCase();
+
+}
+
+let score = 0;
+
+// the game whole logic
+
+function playRound(playerSelection, computerSelection) {
+
+    
+     if (playerSelection == computerSelection) {
+
+        console.log("It's a draw");
+        return console.log(`score is ${score}`);
+
+
+    } else if (playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors" || 
+    playerSelection == "scissors" && computerSelection == "rock") {
+
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+        return console.log(`score is ${score}`);
+
+
+    } else if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || 
+    playerSelection == "scissors" && computerSelection == "paper") {
+        
+
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        score += 1;
+        return console.log(`score is ${score}`);
+ 
+
+    } else {
+
+        console.log("incorrect choice! please choose from the following: 'Rock', 'Paper' or 'Scissors' ");
+        return console.log(`score is ${score}`);
+
+    }
+
+}
+
+function game() {
+
+    console.log(playRound(playerChoice(), computerPlay()));
+    console.log(playRound(playerChoice(), computerPlay()));
+    console.log(playRound(playerChoice(), computerPlay()));
+    console.log(playRound(playerChoice(), computerPlay()));
+    console.log(playRound(playerChoice(), computerPlay()));
+
+    return console.log(`Your final score equals to ${score}`);
+}
+
+game();
